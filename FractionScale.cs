62ad89aac03f3b664567d2a79e34dd0f6d7f44b0
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2025-05-21 11:50:54 -0700

    added namespace for FractionScale

--- FractionScale.cs
+++ FractionScale.cs
@@ -1,69 +1,71 @@
-
-public class FractionScale
+namespace Assets.DMBTools.FractionScale
 {
-    public static FractionScale zero = new FractionScale(0,0);
-    int numerator;
-    readonly int denominator;
+    public class FractionScale
+    {
+        public static FractionScale zero = new FractionScale(0, 0);
+        int numerator;
+        readonly int denominator;
 
-    public FractionScale(int num, int den)
-    {
-        numerator = num;
-        denominator = den;
-    }
-    public FractionScale(int integer) : this(integer, integer) {}
-    public FractionScale(FractionScale f) : this(f.numerator, f.denominator) {}
-    public FractionScale() : this(0,0) {}
+        public FractionScale(int num, int den)
+        {
+            numerator = num;
+            denominator = den;
+        }
+        public FractionScale(int integer) : this(integer, integer) { }
+        public FractionScale(FractionScale f) : this(f.numerator, f.denominator) { }
+        public FractionScale() : this(0, 0) { }
 
-    public int Get()
-    {
-        return numerator;
-    }
-    public int GetNumerator()
-    {
-        return numerator;
-    }
-    public void SetNumerator(int n)
-    {
-        numerator = n;
-    }
-    public int GetDenominator()
-    {
-        return denominator;
-    }
-    public void Decrement()
-    {
-        numerator -= 1;
-    }
-    public void Decrement(int amount)
-    {
-        numerator -= amount;
-    }
-    public void Increment()
-    {
-        numerator += 1;
-    }
-    public void Increment(int amount)
-    {
-        numerator += amount;
-    }
-    public int ToInt()
-    {
-        return numerator/denominator;
-    }
-    public double ToDouble()
-    {
-        return (double) numerator / (double) denominator;
-    }
-    public float ToFloat()
-    {
-        return (float) numerator / (float) denominator;
-    }
-    override public string ToString()
-    {
-        return $"{numerator} / {denominator}";
-    }
-    public bool Full()
-    {
-        return numerator >= denominator;
+        public int Get()
+        {
+            return numerator;
+        }
+        public int GetNumerator()
+        {
+            return numerator;
+        }
+        public void SetNumerator(int n)
+        {
+            numerator = n;
+        }
+        public int GetDenominator()
+        {
+            return denominator;
+        }
+        public void Decrement()
+        {
+            numerator -= 1;
+        }
+        public void Decrement(int amount)
+        {
+            numerator -= amount;
+        }
+        public void Increment()
+        {
+            numerator += 1;
+        }
+        public void Increment(int amount)
+        {
+            numerator += amount;
+        }
+        public int ToInt()
+        {
+            return numerator / denominator;
+        }
+        public double ToDouble()
+        {
+            return (double)numerator / (double)denominator;
+        }
+        public float ToFloat()
+        {
+            return (float)numerator / (float)denominator;
+        }
+        override public string ToString()
+        {
+            return $"{numerator} / {denominator}";
+        }
+        public bool Full()
+        {
+            return numerator >= denominator;
+        }
     }
 }
